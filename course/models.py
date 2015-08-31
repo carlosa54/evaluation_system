f23a27a5bd2ba8522a4f29a7c73bc6d9bf9f7e1d
carlosa54 <carlos_arg5@yahoo.com>
2015-08-31 18:14:45 -0400

    Added a method for displaying ManyToManyField on the admin site

--- course/models.py
+++ course/models.py
@@ -7,6 +7,11 @@ class Group(models.Model):
 	name = models.CharField(max_length=200)
 	student = models.ManyToManyField(User)
 
+	#Method added for dysplaying all students of the group in the admin page ManyToMany 
+	def student_names(self):
+		return ', '.join([a.student for a in self.admins.all()])
+	student_names.short_description = "Student names"
+
 class Course(models.Model):
 	name = models.CharField(max_length=200)
 	section = models.CharField(max_length=3)
