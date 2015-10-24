e30de42fc993ed1b52bc619a30b5673b7a1a4252
carlosa54 <carlos_arg5@yahoo.com>
2015-10-24 12:58:19 -0400

    Register groups to admin

--- evaluation_system/evaluation/admin.py
+++ evaluation_system/evaluation/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Evaluation, Evaluation_Question
+from .models import Evaluation, Evaluation_Question, Group, Group_User
 # Register your models here.
 admin.site.register(Evaluation)
 admin.site.register(Evaluation_Question)
+admin.site.register(Group)
+admin.site.register(Group_User)
