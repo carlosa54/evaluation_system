540e0366f2323cc16508b9bfd4de4b417f349623
carlosa54 <carlos_arg5@yahoo.com>
2015-10-25 14:39:31 -0400

    Changed form of evaluation

--- evaluation_system/evaluation/views.py
+++ evaluation_system/evaluation/views.py
@@ -5,12 +5,23 @@ from .forms import ProfessorEvaluateForm
 
 # Create your views here.
 class ProfessorEvaluateView(TemplateView):
-	template_name = "evaluation/evaluate.html"
+	template_name = "dashboard/evaluate.html"
 
 	def post(self, request, *args, **kwargs):
 		if not request.user.is_authenticated():
 			return redirect("/login")
 		context = self.get_context_data(**kwargs)
+
+		form = ProfessorEvaluateForm(request.POST)
+
+		if form.is_valid():
+			new_evaluation = form.save()
+
+			context["form"] = form
+			context["success"] = "Evaluation created"
+		else:
+			context["form"] = form
+			context["error"] = "Evaluation failed"
 		
 		return self.render_to_response(context)
 
