ff87ba9379d5931bfc7c1707642a8664f3f6d293
carlosa54 <carlos_arg5@yahoo.com>
2015-11-04 20:10:55 -0400

    Fixed Answer and Question

--- evaluation_system/questions/models.py
+++ evaluation_system/questions/models.py
@@ -6,12 +6,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Answer(models.Model):
 	score = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(10)], null=True)
-	answered_by = models.ForeignKey(User, limit_choices_to= {'type':'student'}, related_name = 'answered_by')
-	answered_for = models.ForeignKey(User, limit_choices_to= {'type':'student'}, related_name = 'answered_for')
+	answered_by = models.ForeignKey(User, limit_choices_to= {'type':'student'}, related_name = 'answered_by', null = True, blank = True)
+	answered_for = models.ForeignKey(User, limit_choices_to= {'type':'student'}, related_name = 'answered_for', null = True, blank = True)
 
 class Question(models.Model):
 	question_text = models.TextField()
-	answers = models.ForeignKey(Answer, null= True)
+	answers = models.ForeignKey(Answer, null= True, blank = True)
 
 	def __unicode__(self):
 		return self.question_text
