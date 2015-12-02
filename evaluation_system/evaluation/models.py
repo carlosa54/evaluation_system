2584811245744f5b2337421d1a93bf8d9ec364a4
carlosa54 <carlos_arg5@yahoo.com>
2015-12-02 17:29:34 -0400

    Added done logic to models

--- evaluation_system/evaluation/models.py
+++ evaluation_system/evaluation/models.py
@@ -40,10 +40,27 @@ class Group(models.Model):
 		return ', '.join([a.first_name for a in self.students.all()])
 	student_names.short_description = "Student names"
 
+	@property
+	def done(self):
+		count = len(self.group_user_set.all())
+
+		if count > 1:
+			for student, i in enumerate(self.group_user_set.all()):
+				if not student.done:
+					return False
+				elif i == count:
+					return True
+		elif not self.group_user_set.all()[0].done:
+			return False
+		else:
+			return True
+
+
+
 class Group_User(models.Model):
 	group = models.ForeignKey(Group)
 	student = models.ForeignKey(User, limit_choices_to= {'type':'student'})
-
+	done = models.BooleanField(default = False)
 	def __unicode__(self):
 		return self.group.name
 
